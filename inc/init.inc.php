3fc7beb5c7287e82295c65c102457a686e454171
adrien monsegu <adrienmonsegu@gmail.com>
2020-02-17 11:10:22 +0100

    test inscription fonctionnel init

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -1,28 +1,23 @@
-<?php
+<?php 
+//------- CONNEXION BDD
+$bdd = new PDO('mysql:host=localhost;dbname=swift', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
-// Connexion à la BDD
-$host_db = 'mysql:host=localhost;dbname=swift'; 
-$login = 'root'; 
-$password = ''; 
-$options = array(
-				PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, 
-				PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' 
-				);				
-$pdo = new PDO($host_db, $login, $password, $options);
-
-// Création d'une variable destinée à afficher des messages utilisateur
-$msg = "";
-
-// ouverture d'une session
+//------- SESSION
 session_start();
 
-// déclaration de constante
-// URL racine du projet
-define('URL', 'http://swift/'); // lien absolu racine du projet
-// Chemin racine du serveur
-define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT']);
-// Chemin racine du dossier du site depuis le serveur
-define('SITE_ROOT', '/swift/');
+//------- CONSTANTE
+define("RACINE_SITE", $_SERVER['DOCUMENT_ROOT'] . '/swift/');
+// cette constante retourne le chemin physique du dossier boutique sur le serveur, lors de l'ulpoad d'images, nous aurons besoin du chemin complet vers le dossier photo pour enregistrer la photo
+// echo RACINE_SITE . '<hr>';
 
-//--------- AUTRES INCLUSIONS
-require_once("fonction.inc.php");
+define("URL", "http://localhost/swift/");
+// cette constante servira par exemple à enregistrer l'URL d'une image dans la BDD, on ne conserve pas l'image dans le BDD, ce serait trop lour pour le serveur 
+
+//------- FAILLES XSS 
+foreach($_POST as $key => $value)
+{
+    $_POST[$key] = strip_tags($value);
+}
+
+//------- INCULSIONS 
+require_once("fonction.inc.php"); 
