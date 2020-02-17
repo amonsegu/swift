4ec8cc7b88a9db436dc571d1b8ff8c107c7a6573
adrien monsegu <adrienmonsegu@gmail.com>
2020-02-17 09:48:40 +0100

    mise en place init header footer

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -1,19 +1,85 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Swift Location Salle & bureau</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Swift location bureau & salle</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Boutique démo PHP</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
+    <div class="collapse navbar-collapse" id="navbarsExample04">
+        <ul class="navbar-nav mr-auto">
 
-    <!-- Bootstrap core CSS -->
-	<link href="<?php echo URL; ?>css/bootstrap.min.css" rel="stylesheet">
-	<!-- OWN CSS -->
-	<link href="<?php echo URL; ?>css/style.css" rel="stylesheet">
-	<!-- FONT AWESOME -->
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0/css/all.min.css" rel="stylesheet">
+        <?php if(connect()): // accés membre connecté ?>
 
-  </head>
-  <body>
+            <?php 
+            if(isset($_SESSION['panier']))
+                $nbProduct = array_sum($_SESSION['panier']['quantite']);
+            else
+                $nbProduct = 0;
+            ?>
+
+            <li class="nav-item">
+                <a class="nav-link" href="<?= URL ?>profil.php">Profil</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= URL ?>boutique.php">Boutique</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= URL ?>panier.php">Panier <span class="badge badge-info"><?= $nbProduct ?></span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= URL ?>connexion.php?action=deconnexion">Deconnexion</a>
+            </li>
+        
+        <?php else: // visiteur non connécté et non inscrit ?>
+
+            <?php 
+            if(isset($_SESSION['panier']))
+                $nbProduct = array_sum($_SESSION['panier']['quantite']);
+            else
+                $nbProduct = 0;
+            ?>
+
+            <li class="nav-item">
+                <a class="nav-link" href="<?= URL ?>inscription.php">Inscription</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= URL ?>connexion.php">Connexion</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= URL ?>boutique.php">Boutique</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= URL ?>panier.php">Panier <span class="badge badge-info"><?= $nbProduct ?></span></a>
+            </li>
+
+        <?php endif; ?>
+
+        <?php if(connecteAdmin()): // accés administrateur ?>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">BACKOFFICE</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown04">
+                <a class="dropdown-item" href="<?= URL ?>admin/gestion_boutique.php">Gestion Boutique</a>
+                <a class="dropdown-item" href="<?= URL ?>admin/gestion_membre.php">Gestion Membre</a>
+                <a class="dropdown-item" href="<?= URL ?>admin/gestion_commande.php">Gestion Commande</a>
+                </div>
+            </li>
+
+        <?php endif; ?>
+
+        </ul>
+        <form class="form-inline my-2 my-md-0">
+        <input class="form-control" type="text" placeholder="Search">
+        </form>
+    </div>
+    </nav>
+    <main class="container-fluid" style="min-height: 800px;">
