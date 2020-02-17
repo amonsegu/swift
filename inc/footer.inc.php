5e5a565c51481854521e9aa327d1f5b5374f868c
adrien monsegu <adrienmonsegu@gmail.com>
2020-02-17 09:34:08 +0100

    config initial 2

---
+++ inc/footer.inc.php
@@ -0,0 +1,9 @@
+</main><!-- /.container -->
+
+
+
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="<?php echo URL; ?>js/bootstrap.min.js"></script>
+        <script src="<?php echo URL; ?>js/eboutique.js"></script>
+	</body>
+</html>
