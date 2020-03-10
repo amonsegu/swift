54771c88cb3c2787b5bb7f4999225b1ef74c574d
Adrien Monsegu <adrienmonsegu@gmail.com>
2020-03-10 20:27:33 +0100

    navbar final

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -9,24 +9,31 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
     <link href="https://use.fontawesome.com/releases/v5.0.7/css/all.css" rel="stylesheet" >
-    <title>Swift location bureau & salle</title>
+    <title>Switch location bureau & salle</title>
     <script type="text/javascript" src="inc/js/showproductville"></script>
     <script type="text/javascript" src="inc/js/showproductcp"></script>
     <script type="text/javascript" src="inc/js/showproductcat"></script>
     <script type="text/javascript" src="inc/js/showproductcap"></script>
 </head>
+
+
+
+
 <body style="height:100%">
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Swift</a>
+    
+    
+    
+    <nav class="navbar sticky-top  navbar-expand-md navbar-dark bg-dark">
+    <a class="navbar-brand" href="<?= URL ?>index.php">Switch</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarsExample04">
-        <ul class="navbar-nav mr-auto">
+        
 
         <?php if(connect()): // accés membre connecté ?>
-
+<ul class="navbar-nav mr-auto mt-2  mt-lg-0">
             <?php 
             if(isset($_SESSION['panier']))
                 $nbProduct = array_sum($_SESSION['panier']['quantite']);
@@ -34,60 +41,70 @@
                 $nbProduct = 0;
             ?>
 
-            <li class="nav-item">
-                <a class="nav-link" href="<?= URL ?>profil.php">Profil</a>
-            </li>
-            <li class="nav-item">
+
+            
+            <li class="nav-item active">
                 <a class="nav-link" href="<?= URL ?>index.php">Boutique</a>
             </li>
+            
             <li class="nav-item">
                 <a class="nav-link" href="<?= URL ?>panier.php">Panier <span class="badge badge-info"><?= $nbProduct ?></span></a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= URL ?>connexion.php?action=deconnexion">Deconnexion</a>
-            </li>
-        
-        <?php else: // visiteur non connécté et non inscrit ?>
+            </ul>
 
+
+            <?php if(connecteAdmin()): // accés administrateur ?>
+            <ul class="navbar-nav  my-2 my-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Administration</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown04">
+                    <a class="dropdown-item" href="<?= URL ?>admin/gestion_salle.php">Gestion salle</a>
+                    <a class="dropdown-item" href="<?= URL ?>admin/gestion_produit.php">Gestion Produit</a>
+                    <a class="dropdown-item" href="<?= URL ?>admin/gestion_membre.php">Gestion Membre</a>
+                    <a class="dropdown-item" href="<?= URL ?>admin/gestion_avis.php">Gestion Avis</a>
+                    <a class="dropdown-item" href="<?= URL ?>admin/gestion_commande.php">Gestion Commande</a>
+                    <a class="dropdown-item" href="<?= URL ?>admin/gestion_statistique.php">Gestion Statistique</a>
+                    </div>
+                </li>
+            </ul>
+            <?php endif; ?>          
+            <ul class="navbar-nav  my-2 my-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= URL ?>profil.php" id ="dropdown01" ><i class="fas fa-user" style="color:#fffff;"></i>&nbsp; Profil</a>
+                </li>
+                <li class="nav-item">
+                        <a class="nav-link" href="<?= URL ?>connexion.php?action=deconnexion">Deconnexion</a>
+                    </div>
+                </li>
+           </ul>      
+        <?php else: // visiteur non connécté et non inscrit ?>
+        <ul class="navbar-nav mr-auto mt-2  mt-lg-0"> 
             <?php 
             if(isset($_SESSION['panier']))
                 $nbProduct = array_sum($_SESSION['panier']['quantite']);
             else
                 $nbProduct = 0;
             ?>
+    
+            <li class="nav-item">
+                <a class="nav-link" href="<?= URL ?>index.php">Boutique</a>
+            </li>            
+            
+            <li class="nav-item">
+                <a class="nav-link" href="<?= URL ?>panier.php">Panier <span class="badge badge-info"><?= $nbProduct ?></span></a>
+            </li>
+            
+        </ul>
 
+        <ul class="navbar-nav  my-2 my-lg-0">
             <li class="nav-item">
                 <a class="nav-link" href="<?= URL ?>inscription.php">Inscription</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="<?= URL ?>connexion.php">Connexion</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= URL ?>index.php">Boutique</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="<?= URL ?>panier.php">Panier <span class="badge badge-info"><?= $nbProduct ?></span></a>
-            </li>
-
-        <?php endif; ?>
-
-        <?php if(connecteAdmin()): // accés administrateur ?>
-
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Administration</a>
-                <div class="dropdown-menu" aria-labelledby="dropdown04">
-                <a class="dropdown-item" href="<?= URL ?>admin/gestion_salle.php">Gestion salle</a>
-                <a class="dropdown-item" href="<?= URL ?>admin/gestion_produit.php">Gestion Produit</a>
-                <a class="dropdown-item" href="<?= URL ?>admin/gestion_membre.php">Gestion Membre</a>
-                <a class="dropdown-item" href="<?= URL ?>admin/gestion_avis.php">Gestion Avis</a>
-                <a class="dropdown-item" href="<?= URL ?>admin/gestion_commande.php">Gestion Commande</a>
-                <a class="dropdown-item" href="<?= URL ?>admin/gestion_statistique.php">Gestion Statistique</a>
-                </div>
-            </li>
-
-        <?php endif; ?>
-
         </ul>
+        <?php endif; ?>
     </div>
     </nav>
     <main class="container-fluid" style="min-height:890px">
