eda755a8c38e1167b2be4e0aa1c76b25a567b589
Adrien Monsegu <adrienmonsegu@gmail.com>
2020-03-11 00:21:14 +0100

    maj index et header

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -14,6 +14,7 @@
     <script type="text/javascript" src="inc/js/showproductcp"></script>
     <script type="text/javascript" src="inc/js/showproductcat"></script>
     <script type="text/javascript" src="inc/js/showproductcap"></script>
+    <script type="text/javascript" src="inc/js/test"></script>
 </head>
 
 
--- index.php
+++ index.php
@@ -13,134 +13,143 @@ require_once('inc/header.inc.php');
 <!--container-->
 <div class="container">
 
-<div class="row">
+  <div class="row">
 
-  <div class="col-lg-3">
-
-
- <div class="container">
-   
- <?php $datasalle = $bdd ->query("SELECT DISTINCT ville FROM salle ");?>
-
-
-
-
-  <div class="form-group">
-
-     <!-- VILLE DE LA SALLE-->
-    <label style="margin-top:20px" for="ville" ><b>Ville:</b></label><br>
-    <select name="ville" classe="form-control" onchange="showProductVille(this.value)">
-    <?php
-                while($cat = $datasalle->fetch(PDO::FETCH_ASSOC)):
-            ?>
-                <option value='<?php echo (string)$cat['ville'] ?>'>
-                <?php
-                echo $cat['ville'] ;
-                ?>
-                </option>
-            <?php endwhile; ?>  
-    </select><br>
-
-  
-   <!-- CP DE LA SALLE-->
-   <?php $cpsalle = $bdd ->query("SELECT DISTINCT cp FROM salle ");?>
-    <label style="margin-top:20px" for="cp" ><b>Code postal:</b></label><br>
-    <select name="cp" classe="form-control" onchange="showProductCp(this.value)">
-    <?php
-                while($cat = $cpsalle->fetch(PDO::FETCH_ASSOC)):
-            ?>
-                <option value='<?php echo (int)$cat['cp'] ?>'>
-                <?php
-                echo $cat['cp'] ;
-                ?>
-                </option>
-            <?php endwhile; ?>
-      </select><br>
-     
-   
-  
-  <!-- CATEGORIE DE LA SALLE-->
-  <?php $catsalle = $bdd ->query("SELECT DISTINCT categorie FROM salle ");?>
-  <label style="margin-top:20px" for="cat" ><b>Catégorie:</b></label><br>
- <select name="cat" classe="form-control" onchange="showProductCat(this.value)">
-            <?php
-                while($cat = $catsalle->fetch(PDO::FETCH_ASSOC)):
-            ?>
-                <option value='<?php echo (string)$cat['categorie'] ?>'>
-                <?php
-                echo $cat['categorie'] ;
-                ?>
-                </option>
-            <?php endwhile; ?>
-  </select><br>
-
-
-
-
-  
-  <!-- CAPACITE DE LA SALLE-->
-  <?php $capsalle = $bdd ->query("SELECT DISTINCT capacite FROM salle ");?>
-  <label style="margin-top:20px" for="cap" ><b>Capacité:</b></label><br>
- <select name="cap" classe="form-control" onchange="showProductCap(this.value)">
- <?php
-                while($cat = $capsalle->fetch(PDO::FETCH_ASSOC)):
-            ?>
-                <option value='<?php echo (int)$cat['capacite'] ?>'>
-                <?php
-                echo $cat['capacite'] ;
-                ?>
-                </option>
-            <?php endwhile; ?>
-  </select>
-
- </div>
-
-
-
-
-
-</div>
+    <div class="col-lg-9 ">
+          
+          <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            
+            <div class="carousel-inner" role="listbox">
+              
+              <div class="carousel-item active">
+                <img class="d-block img-fluid" src="photo/ban.jpg" alt="First slide">
+              </div>
+              
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="photo/ban.jpg" alt="Second slide">
+              </div>
+              
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="photo/ban.jpg" alt="Third slide">
+              </div>
+            
+            </div>
+            
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          
+          </div>
+      
+      </div>
 
 
 
 
 
 
+
+    <div class="col-lg-3 ">
+        <!--REQUETE DE SELECTION POUR LES INPUT SELECT-->
+        <?php $datasalle = $bdd ->query("SELECT DISTINCT ville FROM salle ");?>
+        <?php $cpsalle = $bdd ->query("SELECT DISTINCT cp FROM salle ");?>
+        <?php $catsalle = $bdd ->query("SELECT DISTINCT categorie FROM salle ");?>
+        <?php $capsalle = $bdd ->query("SELECT DISTINCT capacite FROM salle ");?>
+
+      <p style="margin-top:30px;text-align:center;" ><b>Menu de navigation rapide</b></p>
+      <p style="text-align:center;">Vos criteres nos dispos</p> 
+      
+      <form>     
+      <!-- VILLE DE LA SALLE-->
+      <div class="form-group">
+          <label for="selectVille">Ville</label>
+          <select class="form-control" id="selectVille" onchange="showProductVille(this.value)">
+          <option value="" selected disabled >selectionner</option>
+          <?php
+                      while($cat = $datasalle->fetch(PDO::FETCH_ASSOC)):
+                  ?>
+                      <option value='<?php echo (string)$cat['ville'] ?>'>
+                      <?php
+                      echo $cat['ville'] ;
+                      ?>
+                      </option>
+                  <?php endwhile; ?> 
+          </select>
         </div>
+
+        <!-- CP DE LA SALLE-->
+        <div class="form-group">
+          <label for="selectcp">Code Postal</label>
+          <select class="form-control" id="selectcp" onchange="showProductCp(this.value)">
+          <option value="" selected disabled >selectionner</option>
+          <?php
+                      while($cat = $cpsalle->fetch(PDO::FETCH_ASSOC)):
+                  ?>
+                      <option value='<?php echo (int)$cat['cp'] ?>'>
+                      <?php
+                      echo $cat['cp'] ;
+                      ?>
+                      </option>
+                  <?php endwhile; ?> 
+          </select>
+        </div>
+
+        <!-- CATEGORIE DE LA SALLE-->
+        <div class="form-group">
+          <label for="selectCat">Categorie</label>
+          <select class="form-control" id="selectCat" onchange="showProductCat(this.value)">
+          <option value="" selected disabled >selectionner</option>
+          <?php
+                      while($cat = $catsalle->fetch(PDO::FETCH_ASSOC)):
+                  ?>
+                      <option value='<?php echo (string)$cat['categorie'] ?>'>
+                      <?php
+                      echo $cat['categorie'] ;
+                      ?>
+                      </option>
+                  <?php endwhile; ?> 
+          </select>
+        </div>
+
+        <!-- CAPACITE DE LA SALLE-->
+          <div class="form-group">
+            <label for="selectSalle">Capacité</label>
+            <select class="form-control" id="selectSalle" onchange="showProductCap(this.value)">
+            <option value="" selected disabled >selectionner</option>
+            <?php
+                        while($cat = $capsalle->fetch(PDO::FETCH_ASSOC)):
+                    ?>
+                        <option value='<?php echo (int)$cat['capacite'] ?>'>
+                        <?php
+                        echo $cat['capacite'] ;
+                        ?>
+                        </option>
+                    <?php endwhile; ?>
+            </select>
+          </div>
+      </form>
+
+  </div>
   <!-- /.col-lg-3 -->
 
-  <div class="col-lg-9">
-
-    <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner" role="listbox">
-        <div class="carousel-item active">
-          <img class="d-block img-fluid" src="photo/ban.jpg" alt="First slide">
-        </div>
-        <div class="carousel-item">
-          <img class="d-block img-fluid" src="photo/ban.jpg" alt="Second slide">
-        </div>
-        <div class="carousel-item">
-          <img class="d-block img-fluid" src="photo/ban.jpg" alt="Third slide">
-        </div>
-      </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </div>
-
-    <div class="row">
-
-
+  
+                        </div>
+  <!--FIN ROW1-->
+  
+  
+  <!--ROW2-->
+  <div class="row" >
 
     <!-- Affichage produit SELECTION AJAX-->
     <div class="container row" id="txtHint" >
